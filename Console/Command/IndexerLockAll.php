5e645fb1f8ffc90133955a00ad8d4c2bdd9d5c56
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2022-11-07 14:39:41 +0100

    Added mode input validation

--- Console/Command/IndexerLockAll.php
+++ Console/Command/IndexerLockAll.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Element119\IndexerDeployConfig\Console\Command;
 
+use InvalidArgumentException;
 use Magento\Framework\App\DeploymentConfig\Writer;
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\Framework\Config\File\ConfigFilePool;
 use Magento\Indexer\Console\Command\AbstractIndexerCommand;
+use Magento\Indexer\Console\Command\IndexerSetModeCommand;
 use Magento\Indexer\Model\Indexer\CollectionFactory;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -34,8 +36,6 @@ class IndexerLockAll extends AbstractIndexerCommand
         $this->configWriter = $configWriter;
     }
 
-    public const MODE_OPTION = 'mode';
-
     /**
      * {@inheritdoc}
      */
@@ -43,7 +43,19 @@ class IndexerLockAll extends AbstractIndexerCommand
         InputInterface $input,
         OutputInterface $output
     ) {
-        $mode = $input->getOption(self::MODE_OPTION);
+        $mode = $input->getOption(IndexerSetModeCommand::INPUT_KEY_MODE);
+
+        $modes = [
+            IndexerSetModeCommand::INPUT_KEY_SCHEDULE,
+            IndexerSetModeCommand::INPUT_KEY_REALTIME
+        ];
+
+        if (!is_null($mode) && !in_array($mode, $modes, true))
+        {
+            throw new InvalidArgumentException(
+                'Passed mode must be one of: ' . implode(', ', $modes)
+            );
+        }
 
         $indexerConfig = [];
 
@@ -64,7 +76,7 @@ class IndexerLockAll extends AbstractIndexerCommand
         $this->setName('indexer:lock-all');
         $this->setDescription('Lock all indexers (default locks to Update on Schedule)');
         $this->setDefinition([
-            new InputOption(self::MODE_OPTION, 'm', InputArgument::OPTIONAL, 'Mode', 'schedule'),
+            new InputOption(IndexerSetModeCommand::INPUT_KEY_MODE, 'm', InputArgument::OPTIONAL, 'Mode', null),
         ]);
         parent::configure();
     }
